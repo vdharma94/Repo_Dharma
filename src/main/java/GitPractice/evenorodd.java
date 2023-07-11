35c940de08b86d4c7416eefb9ea3162bccc1861b
vdharma94 <138884611+vdharma94@users.noreply.github.com>
2023-07-11 17:25:27 +0530

    Create evenorodd.java
    merge1

---
+++ src/main/java/GitPractice/evenorodd.java
@@ -0,0 +1,19 @@
+package GitPractice;
+
+public class evenorodd {
+	
+	public static void main(String[] args) {
+		
+		int n=18;
+		
+		if(n%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+	}
+
+}
