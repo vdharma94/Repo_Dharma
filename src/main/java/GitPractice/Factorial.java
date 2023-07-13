a618234722525269682e9b899fa725780152b5e7
vdharma94 <138884611+vdharma94@users.noreply.github.com>
2023-07-13 16:18:59 +0530

    Create Factorial.java

---
+++ src/main/java/GitPractice/Factorial.java
@@ -0,0 +1,17 @@
+package GitPractice;
+
+public class Factorial {
+	
+	public static void main(String[] args) {
+		
+		int n=16;
+		long s=1L;
+		for(int i=1;i<=n;i++)
+		{
+			s=s*i;	
+		}
+		System.out.println("The Factorial of"+" "+n+" "+"is"+" "+s);
+
+	}
+
+}
