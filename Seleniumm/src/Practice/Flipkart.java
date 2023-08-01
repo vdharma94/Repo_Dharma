d6210c034c3b1dcd99aa16ff32ea384659750606
vdharma94 <138884611+vdharma94@users.noreply.github.com>
2023-08-01 16:35:15 +0530

    commit
    flipkart_becomeseller

--- Seleniumm/src/Practice/Flipkart.java
+++ Seleniumm/src/Practice/Flipkart.java
@@ -33,44 +33,51 @@ public class Flipkart {
 		Thread.sleep(2000);
 		ele_flip.click();
 		
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		
-		Thread.sleep(3000);
 		By by_cross=By.xpath("//button[@class='_2KpZ6l _2doB4z']");
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		WebElement ele_cross=d.findElement(by_cross);
-		Thread.sleep(3000);
-		ele_cross.click();
-		
-		By by_login=By.xpath("//a[text()='Login']");
-		Thread.sleep(3000);
-		WebElement ele_login=d.findElement(by_login);
-		Thread.sleep(3000);
-		ele_login.click();
+		Thread.sleep(2000);
+		ele_cross.click();		
 		
 		Thread.sleep(2000);
-		
-		By by_email=By.xpath("//input[@class='_2IX_2- VJZDxU']");
-		WebElement ele_email=d.findElement(by_email);
-		Thread.sleep(2000);
-		ele_email.sendKeys("996365199");
-		
-		Thread.sleep(3000);
 
-		By by_reqotp=By.xpath("//button[@class='_2KpZ6l _2HKlqd _3AWRsL']");
-		WebElement ele_reqotp=d.findElement(by_reqotp);
+		By by_bseller=By.xpath("//a[@class='_3-PJz-']");
+		WebElement ele_bseller=d.findElement(by_bseller);
 		Thread.sleep(2000);
-		ele_reqotp.click();
+		ele_bseller.click();
 		
-		
-		
-//		By by_bseller=By.xpath("(//span[text()='Become a Seller'])[1]");
-//		WebElement ele_bseller=d.findElement(by_bseller);
-//		Thread.sleep(2000);
-//		ele_bseller.click();
-		
-		
+		Thread.sleep(2000);
 
+		By by_createacc=By.xpath("(//a[text()='Create Account'])[1]");
+		WebElement ele_createacc=d.findElement(by_createacc);
+		Thread.sleep(2000);
+		ele_createacc.click();
+		
+		Thread.sleep(2000);
+
+		By by_listproducts=By.xpath("(//a[text()='List Products'])[1]");
+		WebElement ele_listproducts=d.findElement(by_listproducts);
+		Thread.sleep(2000);
+		ele_listproducts.click();
+		
+		Thread.sleep(2000);
+
+		By by_sands=By.xpath("(//a[text()='Storage & Shipping'])[1]");
+		WebElement ele_sands=d.findElement(by_sands);
+		Thread.sleep(2000);
+		ele_sands.click();
+		
+		Thread.sleep(2000);
+
+		By by_Rp=By.xpath("(//a[text()='Receive Payments'])[1]");
+		WebElement ele_Rp=d.findElement(by_Rp);
+		Thread.sleep(2000);
+		ele_Rp.click();
+		
+		
+		
 
 	}
 
